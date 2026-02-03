0bf5eabadab4cae96865e2511e4e86549ac65e49
andrew.lencmanis <andrew.lencmanis@veset.tv>
2026-02-03 11:15:32 +0200

    fist

--- src/main/java/com/example/common/Application.java
+++ src/main/java/com/example/common/Application.java
@@ -1,9 +1,5 @@
 package com.example.common;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
 //@SpringBootApplication
 //@ConfigurationPropertiesScan
 public class Application {
