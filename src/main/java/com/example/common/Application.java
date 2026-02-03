c489be73309208fb9c1f0d10a6d401bf5406dd5c
andrew.lencmanis <andrew.lencmanis@veset.tv>
2026-02-03 11:15:02 +0200

    fist

--- src/main/java/com/example/common/Application.java
+++ src/main/java/com/example/common/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
-@ConfigurationPropertiesScan
+//@SpringBootApplication
+//@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
     }
 }
