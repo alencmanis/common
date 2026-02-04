66e46b55218fd367f7c50e03328da15ad9dcdd20
andrew.lencmanis <andrew.lencmanis@veset.tv>
2026-02-05 01:11:51 +0200

    2

--- src/main/java/com/example/common/jobs/SelectMaster.java
+++ src/main/java/com/example/common/jobs/SelectMaster.java
@@ -37,9 +37,7 @@ public class SelectMaster {
         this.client = client;
     }
 
-//    @Scheduled(fixedDelayString = "${fly.master.job-delay-ms:300000}")
     public void run() {
-        log.info("log");
         if (!running.compareAndSet(false, true)) {
             return;
         }
